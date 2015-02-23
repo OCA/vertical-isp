4abf70238f3637c2c73c1149e6887226f67fff24
Vincent Vinet <vincent.vinet@savoirfairelinux.com>
2015-02-23 17:31:29 -0500

    enlarge product select in configurator, select child cats

--- contract_isp/__openerp__.py
+++ contract_isp/__openerp__.py
@@ -48,6 +48,9 @@ Features:
              'contract_isp_view.xml',
              'contract_isp_data.xml',
              'contract_isp_workflow.xml'],
+    'css': [
+        'static/src/css/configurator.css',
+    ],
     'active': False,
     'installable': True,
 }
