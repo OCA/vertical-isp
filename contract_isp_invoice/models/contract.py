a13a32f190f046d23abfee15555055072f2bb9ed
punit chaudhary <punit.chaudhary.serpentcs@gmail.com>
2015-10-21 15:35:44 +0530

    optimize the code as per the Travis ci build

--- contract_isp_invoice/models/contract.py
+++ contract_isp_invoice/models/contract.py
@@ -111,15 +111,15 @@ class account_voucher(models.Model):
         voucher = self.browse(self.id)
         if self._context.get('not_subscription_voucher', True) is False:
             if self._context.get('active_model') == 'account.analytic.account'and\
-            self._context.get('active_id', False):
+                self._context.get('active_id', False):
                 for line in account_analytic_account_obj.\
-                    browse(self._context.get('active_id')).contract_service_ids:
+                browse(self._context.get('active_id')).contract_service_ids:
                     line.create_analytic_line(mode='subscription',
                                               date=datetime.datetime.today())
                     inv = account_analytic_account_obj.\
                         create_invoice(self._context.get('active_ids'))
-                        # a = account_invoice_obj.signal_workflow
-                        # ('invoice_open')
+                    # a = account_invoice_obj.signal_workflow
+                    # ('invoice_open')
             else:
                 raise Warning(_('Contract not found'))
 
@@ -323,7 +323,7 @@ class account_analytic_account(models.Model):
 
         amount_tax = amount_untaxed = 0
         for line in self.browse(self._context.get('active_id')).\
-        contract_service_ids:
+            contract_service_ids:
             for c in self.env['account.tax'].compute_all(line.product_id.
                                                          taxes_id.id,
                                                          line.unit_price or
