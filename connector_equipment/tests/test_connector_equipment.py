4f9ee880797836c1712696ae441d3d845b2ab6a5
Daniel Reis <dreis@opensourceintegrators.com>
2019-10-16 09:33:10 +0100

    [FIX] Remove unused import
    Co-Authored-By: Bhavesh Odedra <bodedra@opensourceintegrators.com>

--- connector_equipment/tests/test_connector_equipment.py
+++ connector_equipment/tests/test_connector_equipment.py
@@ -1,7 +1,6 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import time
-import datetime
 from odoo import fields
 from odoo.tests.common import TransactionCase, Form
 
